9f61e053254552768c281164fe56e3ff51f34676
Dwezil R D'souza <dsouza0@purdue.edu>
2015-12-08 06:45:37 -0500

    Part 1 of shorest path algorithm (update weights) done

--- Project3/adjacent.c
+++ Project3/adjacent.c
@@ -8,31 +8,31 @@ typedef struct coordinates{
 }coordinates;
 
 struct listNode{
-  int node;
-  int weight;
+  unsigned int node;
+  unsigned int weight;
  	struct listNode* next;
 };
 
 typedef struct listNode listNode;
+/*
+struct done{
+	int vertex;
+	short weight;
+	unsigned done;
+};
 
-listNode* createListNode(int nodeNum, int weight)
-{
-	listNode* tmp = (listNode*) malloc(sizeof(listNode));
-	tmp->node = nodeNum;
-	tmp->weight = weight;
-	tmp->next = NULL;
-	
-	return tmp;
-}
+typedef struct minPath minPath;
 
-//function to compute the Euclidean distance between 2 coordinates
-int euclidean(int x1, int y1, int x2, int y2)
-{
-	double distance = (pow((double)(x2 - x1),2) + pow((double)(y2 - y1),2));
-	
-	distance = sqrt((double)distance);
-  return (int)distance;
-}
+void initMinPaths(minPath* minPaths, int numVertices, int rootVertex);
+*/
+
+void initWeights(listNode* ajaList, int vertices, int root);
+
+listNode* createListNode(int nodeNum, int weight);
+
+int euclidean(int x1, int y1, int x2, int y2);
+
+void updateWeights(listNode* ajaList, int currentVertex, unsigned char* done);
 
 int main(int argc,char** argv)
 {	
@@ -60,14 +60,14 @@ int main(int argc,char** argv)
 	}
 	if(feof (fp)) return -1;
 	
-	//-----------Print Coordinates List----
+	/*-----------Print Coordinates List----
 	printf("\nCoordinates List: \n");
 	for(i = 0;i <vertices; i++)
 	{
 		printf("%d: %d %d\n",i,(cords + i)->x,(cords + i)->y); 
 	}
 	
-	//------------------------------------------------
+	//------------------------------------------------*/
 	
 	
 	//----------Create adjacency list-----------------
@@ -136,15 +136,178 @@ int main(int argc,char** argv)
 		}
 		printf("\n");
 	}	
+ /* 
+  //---------CREATE minPATHS array-------------
+  minPath* minPaths = (minPath*)malloc(vertices*sizeof(minPath));
   
-  
-  
-/*
+
+  unsigned int a = -1;
+  unsigned short b = -1;
+  unsigned char c = -1;
+  printf("%u,%u,%u",a,b,c);  
+
 	int x1=0, x2=4,y1=0,y2=4;
 
 	printf("\nThe distance is %d",euclidean(x1,y1,x2,y2));
 */
+	int root = 0;
+	initWeights(ajaList, vertices, root);
+
+		//--------PRINT OUT INIT WEIGHTS--
+	printf("\n\nInit Weights:\n");
+	for(i= 0; i< vertices; i++)
+	{
+		printf("%d(%u)\n",i,ajaList[i].weight);
+	}	
+	
+	//------create/init 'DONE' array on heap
+	unsigned char* visited = (unsigned char*)calloc(vertices, sizeof(unsigned char)); //use calloc to init to zeros(not visited)
+	  
+	//--------PRINT OUT INIT VISITED--
+	printf("\n\nInit Visited :\n");
+	for(i= 0; i< vertices; i++)
+	{
+		printf("%d(%u)\n",i,visited[i]);
+	}	
+	
+	unsigned int min = -1;
+	unsigned int min2 = -1;
+	
+	int currentVertex= 0;
+	updateWeights(ajaList, currentVertex, visited);
+	
+	//Mark current vertex as 'visited'
+	visited[currentVertex] = 1;
+	
+	
+		//--------PRINT OUT WEIGHTS--
+	printf("\n\nUpdated Weights:\n");
+	for(i= 0; i< vertices; i++)
+	{
+		printf("%d(%u)\n",i,ajaList[i].weight);
+	}	
+		
+	//--------PRINT OUT VISITED--
+	printf("\n\nUpdated Visited :\n");
+	for(i= 0; i< vertices; i++)
+	{
+		printf("%d(%u)\n",i,visited[i]);
+	}	
+	
+//------------------TEST OUT UPDATEWIEGHTS-----------	
+	int verts[] = {1,2,3};
+	int j;
+	for(j =0;j<3; j++)
+	{	
+		currentVertex = verts[j];
+		updateWeights(ajaList, currentVertex, visited);
+	
+		//Mark current vertex as 'visited'
+		visited[currentVertex] = 1;
+	
+	
+			//--------PRINT OUT WEIGHTS--
+		printf("\n\nUpdated Weights:\n");
+		for(i= 0; i< vertices; i++)
+		{
+			printf("%d(%u)\n",i,ajaList[i].weight);
+		}	
+		
+		//--------PRINT OUT VISITED--
+		printf("\n\nUpdated Visited :\n");
+		for(i= 0; i< vertices; i++)
+		{
+			printf("%d(%u)\n",i,visited[i]);
+		}	
+		
+	}
+//-------------------------------------------------------	
+	
+	//printf("\n\n\n char-%lu, short-%lu, int-%lu, long-%lu\n",sizeof(char),sizeof(short),sizeof(int),sizeof(long));
+	
+
+	
 	fclose(fp);
 
 	return 0;
+}//END OF MAIN
+
+
+
+
+
+//function to compute the Euclidean distance between 2 coordinates
+int euclidean(int x1, int y1, int x2, int y2)
+{
+	double distance = (pow((double)(x2 - x1),2) + pow((double)(y2 - y1),2));
+	
+	distance = sqrt((double)distance);
+  return (int)distance;
+}
+
+
+listNode* createListNode(int nodeNum, int weight)
+{
+	listNode* tmp = (listNode*) malloc(sizeof(listNode));
+	tmp->node = nodeNum;
+	tmp->weight = weight;
+	tmp->next = NULL;
+	
+	return tmp;
+}
+
+/*
+void initMinPaths(minPath* minPaths, int numVertices, int rootVertex)
+{
+	int i;
+	for(i= 0; i<numVertices; i++)
+	{
+		minPaths[i].vertex = i;
+		minPaths[i].done = 0;
+		
+		if(i == rootVertex) minPaths[i].weight = 0;		
+		else minPaths[i].weight = -1;
+	}
+}
+*/
+
+//This function initializes the ajacency list to prep for Dijkstra's shortest path algorithm from vertex 'root' (weight of root = 0, all other vertices = inf )
+void initWeights(listNode* ajaList, int vertices, int root)
+{
+	int i;
+	for(i= 0; i<vertices; i++)
+	{
+		if(i == root) ajaList[i].weight = 0;
+		else ajaList[i].weight = -1;
+	}
+}
+
+//Part 1 of my iterative implementation of Dijkstra's shortest path algorithm
+//This function looks at all neighbours of a vertex and checks for shorter paths to them 
+void updateWeights(listNode* ajaList, int currentVertex, unsigned char* done)
+{
+	listNode* ajaPtr = ajaList[currentVertex].next; //init to first neighbour
+	unsigned int rootWeight = ajaList[currentVertex].weight; //save weight of current Vertex
+	
+	unsigned int newWeight, oldWeight;
+	
+	while(ajaPtr !=NULL)
+	{
+		printf("\n Iter");
+		
+		if(!(done[ajaPtr->node])) //Ignore nodes already visted 
+		{
+			printf("iter ");
+			newWeight = ajaPtr->weight;
+			oldWeight = ajaList[ajaPtr->node].weight;
+		
+			printf("\noldweight:%u  , newweight:%u",oldWeight,newWeight);
+		
+			if((rootWeight + newWeight) < (oldWeight)) //if shorter path is found
+			{
+				ajaList[ajaPtr->node].weight = rootWeight + newWeight; //update path length
+			}
+		}
+		ajaPtr = ajaPtr->next; //update ajacency list pointer	
+	} 
 }
